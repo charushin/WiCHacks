2a1f77653cb7ac02768cb43290a421bff14aa033
swatishr <swatishr@buffalo.edu>
2018-02-10 22:05:42 -0500

    Post activity done

--- Spunk/app/src/main/java/com/example/android/spunk/SplashActivity.java
+++ Spunk/app/src/main/java/com/example/android/spunk/SplashActivity.java
@@ -20,6 +20,6 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 startActivity(new Intent(getApplicationContext(), HomeActivity.class));
             }
-        }, 5000);
+        }, 3000);
     }
 }
